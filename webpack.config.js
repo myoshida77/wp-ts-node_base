8daee01c5fd835d35110e83de21f43e7ed4b00ab
myoshidq <yoshida.masaki.67w@gmail.com>
2021-04-13 14:17:57 +0900

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const config = {
+  mode: 'development',
+  entry: './src/main.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  },
+  module: {
+    rules: [
+      { // If an extension is .ts,
+        test: /\.ts$/,
+        // compile TypeScript.
+        loader: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  // unnecessary to write the extension in import statement
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+    port: 3000,
+    open: true
+  }
+};
+
+module.exports = config;
